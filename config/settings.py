2fcd12734ce4d14af4195d6df5465c9e8a8b2f53
Bita <bitafarezi79@gmail.com>
2026-06-12 23:05:02 +0330

    Register models in supernatural_users/admin.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Mystic Falls supernatural apps
     'entries',
     'supernatural_users',
 ]
