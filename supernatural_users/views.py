a19a5d3f9349d8453564ecf0f3fab47a0ff6f4c3
Bita <bitafarezi79@gmail.com>
2026-06-13 00:02:34 +0330

    Add view to handle the signup submission

--- supernatural_users/views.py
+++ supernatural_users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import SupernaturalSignupForm
 # Create your views here.
+
+
+class SignupView(View):
+    template_name = 'supernatural_users/signup.html'
+
+    def get(self, request):
+        form = SupernaturalSignupForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = SupernaturalSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to the login page immediately after a successful signup
+            return redirect('login') 
+        return render(request, self.template_name, {'form': form})
