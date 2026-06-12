8220f33e7abe8afd84f4b4b0bed3950eb2346fa9
Bita <bitafarezi79@gmail.com>
2026-06-12 22:55:18 +0330

    Add apps inside INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'entries',
+    'supernatural_users',
 ]
 
 MIDDLEWARE = [
